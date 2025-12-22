5628225a56eb9d70277de16586cfa3c61d69ce3d
Yash Satish Dubey <dubeyyash310@gmail.com>
2025-12-22 10:55:05 +0530

    Implement minDeletionSize function in DeleteColumnsToMakeSorted

---
+++ Leetcode/Easy/DeleteColumnsToMakeSorted.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        if(strs.size() == 0) return 0;
+        int count = 0;
+        for (int i = 0; i < strs[0].length(); i++) {
+            for (int j = 1; j < strs.size(); j++) {
+                if (strs[j][i] < strs[j - 1][i]) {
+                    count++;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+};
