bfd029d60fa7ba739fabed327eea4b2817e0d471
Yash Satish Dubey <dubeyyash310@gmail.com>
2025-12-25 15:14:43 +0530

    Implement Coin Change solution in C++

---
+++ Leetcode/Medium/Coin Change.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, INT_MAX - 1));
+
+        for(int i = 0; i < n + 1; i++){
+            dp[i][0] = 0;
+        }
+
+        for(int j = 1; j < amount + 1; j++){
+            if(j % coins[0] == 0){
+                dp[1][j] = j / coins[0];
+            }
+        }
+
+        for(int i = 2; i < n + 1; i++){
+            for(int j = 1; j < amount + 1; j++){
+                if(coins[i - 1] <= j){
+                    dp[i][j] = min(dp[i-1][j], 1 + dp[i][j - coins[i - 1]]);
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+
+        if(dp[n][amount] >= INT_MAX - 1) return -1;
+        return dp[n][amount];
+    }
+};
