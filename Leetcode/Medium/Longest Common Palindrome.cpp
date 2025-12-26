4bb0e11019e3cc24ae39d8a30b1bea71608da92a
Yash Satish Dubey <dubeyyash310@gmail.com>
2025-12-26 16:12:27 +0530

    Implement longestPalindromeSubseq function

---
+++ Leetcode/Medium/Longest Common Palindrome.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+        string t = "";
+        for(int i = s.length() - 1; i >= 0; i--){
+            t += s[i];
+        }
+        int m = s.length();
+        int n = t.length();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        for(int i = 1; i < m + 1; i++){
+            for(int j = 1; j < n + 1; j++){
+                if(s[i - 1] == t[j - 1]) dp[i][j] = dp[i - 1][j - 1] + 1;
+                else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[m][n];
+    }
+};
