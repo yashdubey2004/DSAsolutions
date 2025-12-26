1d84e05e756c22166197db5093123f0719a77a6c
Yash Satish Dubey <dubeyyash310@gmail.com>
2025-12-26 13:07:39 +0530

    Implement longest common substring function

---
+++ GFG/LongestCommonSubstring.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    int longestCommonSubstr(string& s1, string& s2) {
+        int m = s1.length();
+        int n = s2.length();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        int ans = 0;
+
+        for(int i = 1; i < m + 1; i++){
+            for(int j = 1; j < n + 1; j++){
+                if(s1[i - 1] == s2[j - 1]){
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                    ans = max(ans, dp[i][j]);
+                }
+                else{
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        return ans;
+    }
+};
