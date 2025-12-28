08e44a2bb7a91b0d5e649a21a2f1d53a9ceb9a16
Yash Satish Dubey <dubeyyash310@gmail.com>
2025-12-29 02:43:27 +0530

    Create LongestRepeatingSubsequence.cpp

---
+++ GFG/LongestRepeatingSubsequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    int LongestRepeatingSubsequence(string &s) {
+        // Code here
+        int n = s.length();
+        vector<vector<int>> dp(n + 1, vector<int> (n + 1, 0));
+        for(int i = 1; i < n + 1; i++){
+            for(int j = 1; j < n + 1; j++){
+                if(s[i - 1] == s[j - 1] && i != j){
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }
+                else{
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[n][n];
+    }
+};
