17cc96cd9b25b14ac0653df1ca6c93dde916ae93
Yash Satish Dubey <dubeyyash310@gmail.com>
2026-02-03 16:02:33 +0530

    Create Minimize Maximum Pair Sum in Array.cpp

---
+++ Leetcode/Medium/Minimize Maximum Pair Sum in Array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minPairSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int left = 0, right = nums.size() - 1;
+        int res = INT_MIN;
+        while(left < right){
+            int sum = nums[left] + nums[right];
+            res = max(res, sum);
+            left++;
+            right--;
+        }
+        return res;
+    }
+};
