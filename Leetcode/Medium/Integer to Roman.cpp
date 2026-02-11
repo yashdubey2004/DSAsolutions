6eec9daed2a2423ecb8df5bd18c96e9680376ef6
Yash Satish Dubey <dubeyyash310@gmail.com>
2026-02-11 11:22:42 +0530

    Implement integer to Roman conversion

---
+++ Leetcode/Medium/Integer to Roman.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        string res = "";
+        while(num > 0){
+            if(num >= 1000){
+                res += 'M';
+                num -= 1000;
+            }
+            else if(num >= 900){
+                res += 'C';
+                res += 'M';
+                num -= 900;
+            }
+            else if(num >= 500){
+                res += 'D';
+                num -= 500;
+            }
+            else if(num >= 400){
+                res += 'C';
+                res += 'D';
+                num -= 400;
+            }
+            else if(num >= 100){
+                res += 'C';
+                num -= 100;
+            }
+            else if(num >= 90){
+                res += 'X';
+                res += 'C';
+                num -=  90;
+            }
+            else if(num >= 50){
+                res += 'L';
+                num -= 50;
+            }
+            else if(num >= 40){
+                res += 'X';
+                res += 'L';
+                num -= 40;
+            }
+            else if(num >= 10){
+                res += 'X';
+                num -= 10;
+            }
+            else if(num >= 9){
+                res += 'I';
+                res += 'X';
+                num -= 9;
+            }
+            else if(num >= 5){
+                res += 'V';
+                num -= 5;
+            }
+            else if(num >= 4){
+                res += 'I';
+                res += 'V';
+                num -= 4;
+            }
+            else{
+                res += 'I';
+                num -= 1;
+            }
+        }
+        return res;
+    }
+};
