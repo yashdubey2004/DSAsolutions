c3b438c9b2ee42139a798f2c8d9bf743dd673991
Yash Satish Dubey <dubeyyash310@gmail.com>
2026-02-12 22:05:00 +0530

    Create Time Needed to Buy Tickets.cpp

---
+++ Leetcode/Easy/Time Needed to Buy Tickets.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int count = 0;
+        int i = 0;
+        int n = tickets.size();
+        while(tickets[k] > 0){
+            if(tickets[i] > 0){
+                tickets[i]--;
+                count++;
+            }
+            i++;
+            if(i == n){
+                i = 0;
+            }
+        }
+        return count;
+    }
+};
