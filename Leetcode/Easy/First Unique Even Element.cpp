09f9d1252f4da7e7d414ed74957040f06d799a77
Yash Satish Dubey <dubeyyash310@gmail.com>
2026-03-20 12:58:20 +0530

    Create First Unique Even Element.cpp

---
+++ Leetcode/Easy/First Unique Even Element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int firstUniqueEven(vector<int>& nums) {
+        unordered_map<int, int> freq;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] % 2 == 0) freq[nums[i]]++;
+        }
+        for(int i = 0; i < nums.size(); i++){
+            if(freq[nums[i]] == 1) return nums[i];
+        }
+        return -1;
+    }
+};
