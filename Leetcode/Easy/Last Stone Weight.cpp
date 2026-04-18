0eb5214a4ac7710eb49e25f1251960eb24782c93
Yash Satish Dubey <dubeyyash310@gmail.com>
2026-04-19 03:46:50 +0530

    Create Last Stone Weight.cpp

---
+++ Leetcode/Easy/Last Stone Weight.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq(stones.begin(), stones.end());
+        while(pq.size() > 1){
+            int a = pq.top();
+            pq.pop();
+            int b = pq.top();
+            pq.pop();
+            if(a != b) pq.push(a - b);
+        }
+        if(pq.empty()) return 0;
+        return pq.top();
+    }
+};
