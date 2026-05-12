04e96d4a3d9244e8df3743a5095c959e3cbdc694
Yash Satish Dubey <dubeyyash310@gmail.com>
2026-05-12 21:15:38 +0530

    Create Maximum Subarray.cpp

---
+++ Leetcode/Medium/Maximum Subarray.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int res = nums[0];
+        int sum = 0;
+        for(int i = 0; i < nums.size(); i++){
+            sum += nums[i];
+            if(sum > res) res = sum;
+            if(sum < 0) sum = 0;
+        }
+        return res;
+    }
+};
