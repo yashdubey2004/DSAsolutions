077280f5630e5c791357686ac9d9f9e647a306c6
Yash Satish Dubey <dubeyyash310@gmail.com>
2026-07-27 00:19:10 +0530

    Create Frog Jump.cpp

---
+++ GFG/Frog Jump.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    int minCost(vector<int>& height) {
+        // Code here
+        int n = height.size();
+        vector<int> dp(n, -1);
+        int res = INT_MAX;
+        dp[0] = 0;
+        for(int i = 1; i < n; i++){
+            int fs = dp[i - 1] + abs(height[i] - height[i - 1]);
+            int ss = INT_MAX;
+            if(i > 1) ss = dp[i - 2] + abs(height[i] - height[i - 2]);
+            dp[i] = min(fs, ss);
+        }
+        return dp[n - 1];
+    }
+};
